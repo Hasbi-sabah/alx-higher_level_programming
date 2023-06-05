2fbfd3a8957bad098927e5f98a698b6829b60a19
H. Sabah <sabahhasbi00@gmail.com>
2023-06-05 03:39:34 -0700

    mod in 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,7 +11,7 @@ int check_cycle(listint_t *list)
 
 	if (!list)
 		return (0);
-	for (single = dble = list; single && dble;)
+	for (single = dble = list; single && dble && dble->next;)
 	{
 		single = single->next;
 		dble = dble->next->next;
