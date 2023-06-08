2f1b2c8bbe42a525a937b076fd28933845ee08c1
H. Sabah <sabahhasbi00@gmail.com>
2023-06-08 02:09:02 -0700

    importing in 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,11 +2,11 @@
 # This is a program that imports functions
 # from a file and does some Maths.
 
-import calculator_1 as c1
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, c1.add(a, b)))
-    print("{} + {} = {}".format(a, b, c1.sub(a, b)))
-    print("{} + {} = {}".format(a, b, c1.mul(a, b)))
-    print("{} + {} = {}".format(a, b, c1.div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
