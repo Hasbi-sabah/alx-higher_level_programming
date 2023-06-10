d8224ec966c5f293009bb20a46b86e3f2f3b7b2f
H. Sabah <sabahhasbi00@gmail.com>
2023-06-10 02:39:33 -0700

    modified 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,30 +1,51 @@
 #include "lists.h"
 
 /**
+ * reverse_listint - reverses a listint
+ * @head: head of listint
+ */
+void reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next = NULL;
+
+	while (current)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+}
+
+/**
  * is_palindrome - checks if linked list is a palindome
  * @head: listint_t
  * Return: 0 or 1
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp = *head;
-	int i, j, *arr;
+	listint_t *single, *dble, *rev_head;
 
-	if (!*head)
+	if (!*head || !(*head)->next)
 		return (1);
-	for (i = 0; temp; temp = temp->next, i++)
+	single = dble = *head;
+	for (single = dble = *head; dble && dble->next;
+			dble = dble->next->next, single = single->next)
+		;
+	if (dble)
+		rev_head = single->next;
+	else
+		rev_head = single->next->next;
+	reverse_listint(&rev_head);
+	for (single = *head; rev_head && single; rev_head = rev_head->next,
+			single = single->next)
 	{
-		arr = realloc(arr, i * sizeof(int *));
-		arr[i] = temp->n;
-	}
-	for (j = 0; j < i / 2; j++)
-	{
-		if (arr[j] != arr[i - j - 1])
-		{
-			free(arr);
+		if (single->n != rev_head->n)
 			return (0);
-		}
 	}
-	free(arr);
 	return (1);
 }
