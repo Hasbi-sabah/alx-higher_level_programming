bd3f3e64c6b01ee39e58bd9b3fb95c9ab7d3abbc
H. Sabah <sabahhasbi00@gmail.com>
2023-06-10 00:19:11 -0700

    minor mods in 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * is_palindrome - checks if linked list is a palindome
+ * @head: listint_t
+ * Return: 0 or 1
+ */
 int is_palindrome(listint_t **head)
 {
 	listint_t *temp = *head;
@@ -15,7 +20,11 @@ int is_palindrome(listint_t **head)
 	for (j = 0; j < i / 2; j++)
 	{
 		if (arr[j] != arr[i - j - 1])
+		{
+			free(arr);
 			return (0);
+		}
 	}
+	free(arr);
 	return (1);
 }
