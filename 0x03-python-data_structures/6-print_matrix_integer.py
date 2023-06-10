0c67541c238433bca434e229212dc11e10131a14
H. Sabah <sabahhasbi00@gmail.com>
2023-06-09 23:26:52 -0700

    petty

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#This is a function that prints a matrix of integers.
+# This is a function that prints a matrix of integers.
 
 def print_matrix_integer(matrix=[[]]):
     if matrix == [[]]:
