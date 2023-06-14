a748951915489eeaad3bab47b97e8985da32b30a
H. Sabah <sabahhasbi00@gmail.com>
2023-06-14 05:16:42 -0700

    mod in 13

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,20 +5,12 @@ def roman_to_int(roman_string):
     integer = 0
     roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     save = 0
-    flag = 0
     for n in roman_string:
-        if roman_string.index(n) == 0:
-            save = roman[n]
-            flag = 1
-            continue
         n = roman[n]
-        if flag:
-            n, save = save, n
         if n >= save:
             integer += n
         else:
             integer -= save
             save = n
-            flag = 0
     integer += save
-    return abs(integer)
+    return integer
