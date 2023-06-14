edafc860e7ab0583141b24c2ce8a27d4d867a63c
H. Sabah <sabahhasbi00@gmail.com>
2023-06-14 04:09:01 -0700

    key = lambda

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    sorted_vals = dict(sorted(a_dictionary.items(), lambda x: x[1]))
+    sorted_vals = dict(sorted(a_dictionary.items(), key=lambda x: x[1]))
     return list(sorted_vals)[-1]
