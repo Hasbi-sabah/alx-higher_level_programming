1a1bc49f9dbfbe70ba51fe25d572ff78adb3dc7e
H. Sabah <sabahhasbi00@gmail.com>
2023-06-14 12:41:46 -0700

    upper

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,9 +6,9 @@ def roman_to_int(roman_string):
     roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     save = 0
     for n in reversed(roman_string):
-        if n not in roman:
+        if n.upper() not in roman:
             return None
-        n = roman[n]
+        n = roman[n.upper()]
         if n >= save:
             integer += n
         else:
