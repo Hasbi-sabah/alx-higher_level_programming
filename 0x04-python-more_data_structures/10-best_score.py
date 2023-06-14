3af640053e9c23375fe44fd87e51484edf4c628e
H. Sabah <sabahhasbi00@gmail.com>
2023-06-14 03:53:53 -0700

    file 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    sorted_vals = dict(sorted(a_dictionary.items()))
+    return list(sorted_vals)[-1]
