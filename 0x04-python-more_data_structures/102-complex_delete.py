f3d64981cb0fbb8dcae4de869a0c9af89617161d
H. Sabah <sabahhasbi00@gmail.com>
2023-06-14 13:13:50 -0700

    file 102

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    black_list = []
+    for x, y in a_dictionary.items():
+        if value is y:
+            black_list.append(x)
+    for i in reversed(black_list):
+            del a_dictionary[i]
+    return a_dictionary
