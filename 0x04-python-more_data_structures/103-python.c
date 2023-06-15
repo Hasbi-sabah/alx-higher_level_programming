74472e466de779f8d93b97eaaed6de38f8072d8b
H. Sabah <sabahhasbi00@gmail.com>
2023-06-15 00:22:15 +0000

    less prints

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -22,7 +22,7 @@ void print_python_bytes(PyObject *p)
 		printf("  first 10 bytes:");
 	else
 		printf("  first %ld bytes:", ++size);
-	for (i = 0; i <= size && i < 10; i++)
+	for (i = 0; i < size && i < 10; i++)
 		printf(" %02hhx", trying_str[i]);
 	printf("\n");
 }
