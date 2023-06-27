f18810f441ddd0a5f0a959ee4b222419e05942e3
H. Sabah <sabahhasbi00@gmail.com>
2023-06-27 03:57:11 -0700

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module creates an empty class Square.
+"""
+
+
+class Square:
+    """
+    An empty class named Square.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new private instance of the Square class.
+
+        Args:
+            _Square__size (int): Size of square.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = size
