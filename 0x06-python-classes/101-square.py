65e6e127319732e69b5ea39c5ad3a3f4c0b737d8
H. Sabah <sabahhasbi00@gmail.com>
2023-06-27 08:41:16 -0700

    new line

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -80,7 +80,7 @@ class Square:
     def __str__(self):
         s = ''
         if self.__size == 0:
-            return
+            return ''
         for i in range(self.__position[1]):
             s += '\n'
         for i in range(self.__size):
