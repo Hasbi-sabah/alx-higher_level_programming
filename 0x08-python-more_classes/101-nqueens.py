f0bae2b4d51e8f279a436a0ce7b458f2a39ee21a
H. Sabah <sabahhasbi00@gmail.com>
2023-07-03 11:14:05 -0700

    backtrack

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,34 +1,57 @@
 #!/usr/bin/python3
 from sys import argv
+
+
 def check_if_safe(i, j, board):
+    """
+    Args:
+        i : Argument
+        j : Argument
+        board : Argument
+
+    """
     for element in board:
-        if i == element[0] or j == element[1] or abs(i - element[0]) == abs(j - element[1]):
-                return False
+        if (
+            i == element[0]
+            or j == element[1]
+            or abs(i - element[0]) == abs(j - element[1])
+        ):
+            return False
     return True
 
-def check_if_not_done(i, j, big_board):
-    for board in big_board:
-        if len(board) > i and i == board[i][0] and j == board[i][1]:
-                return False
-    return True
+
+def backtrack(n, row, q_board, s_board):
+    """
+    Args:
+        n : Argument
+        row : Argument
+        q_board : Argument
+        s_board : Argument
+
+    """
+    if row == n:
+        s_board.append(q_board.copy())
+        return
+
+    for column in range(n):
+        if check_if_safe(row, column, q_board):
+            q_board.append([row, column])
+            backtrack(n, row + 1, q_board, s_board)
+            q_board.pop()
+
 
 if len(argv) != 2:
-    print('Usage: nqueens N')
+    print("Usage: nqueens N")
     exit(1)
 if not argv[1].isdigit():
-    print('N must be a number')
+    print("N must be a number")
     exit(1)
 n = int(argv[1])
 if n < 4:
-    print('N must be at least 4')
+    print("N must be at least 4")
     exit(1)
-big_board = []
-for a in range(n):
-    board = []
-    for i in range(n):
-        for j in range(n):
-            if check_if_not_done(i, j, big_board) and check_if_safe(i, j, board):
-                board.append([i, j])
-    big_board.append(board)
-    if len(board) == n:
-        print(board)
+solution_board = []
+queen_board = []
+backtrack(n, 0, queen_board, solution_board)
+for row in solution_board:
+    print(row)
