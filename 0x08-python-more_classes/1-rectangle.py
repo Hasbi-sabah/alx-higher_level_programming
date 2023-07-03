bc6c8ff4ff1ad4e0c59b9384ca5f962732032352
H. Sabah <sabahhasbi00@gmail.com>
2023-07-03 02:36:02 -0700

    more confususion

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,8 +21,8 @@ class Rectangle:
                 (default is 0)
 
         """
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
