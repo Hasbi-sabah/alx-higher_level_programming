154251c13ecd4974189fb00616ea05decaa67379
H. Sabah <sabahhasbi00@gmail.com>
2023-07-03 04:12:47 -0700

    i am confusion

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -28,7 +28,7 @@ class Rectangle:
         """
         self.height = height
         self.width = width
-        Rectangle.number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
