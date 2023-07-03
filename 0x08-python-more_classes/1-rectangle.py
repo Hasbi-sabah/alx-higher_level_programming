482043e829f39b943af0a992fd936a0836d14890
H. Sabah <sabahhasbi00@gmail.com>
2023-07-03 02:26:08 -0700

    typos

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,8 +21,8 @@ class Rectangle:
                 (default is 0)
 
         """
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
@@ -73,7 +73,7 @@ class Rectangle:
 
         """
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
