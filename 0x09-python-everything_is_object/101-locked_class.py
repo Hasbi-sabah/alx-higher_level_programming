11a7573ecdac66bf645d148a9a72783fe641390c
H. Sabah <sabahhasbi00@gmail.com>
2023-07-04 04:42:00 -0700

    petty oh petty

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 class LockedClass:
-   def __setattr__(self, attr, name):
-       if attr != "first_name":
-           raise AttributeError("'LockedClass' object has no attribute {}".format(attr))
-       self.__dict__[attr] = name
+    def __setattr__(self, attr, name):
+        """
+        Args:
+            self : Argument
+            attr : Argument
+            name : Argument
+
+        """
+        if attr != "first_name":
+            raise AttributeError(
+                "'LockedClass' object has no attribute {}".format(attr)
+            )
+        self.__dict__[attr] = name
