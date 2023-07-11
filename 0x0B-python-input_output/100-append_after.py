9816cb9a190f60a1b651dbff383cccf35b4709b8
H. Sabah <sabahhasbi00@gmail.com>
2023-07-11 09:08:32 -0700

    task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module for the function append_after()
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    A function that inserts a line of text to a file,
+    after each line containing a specific string
+
+    Args:
+        filename (str)
+        search_string (str)
+        new_string (str)
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        text = f.readlines()
+    for num, line in enumerate(text, 1):
+        if search_string in line:
+            text[num:num] = new_string
+    with open(filename, "w", encoding="utf-8") as f:
+        f.writelines(text)
