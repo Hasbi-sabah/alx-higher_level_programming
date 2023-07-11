424c69d4d6deb6090c67e8a753b51118b5a8d736
H. Sabah <sabahhasbi00@gmail.com>
2023-07-11 03:06:18 -0700

    brainless activities corrected

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,5 +11,5 @@ def read_file(filename=""):
     Args:
         filename (str): default ""
     """
-    with open("my_file_0.txt", "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end='')
