8c2cdf80d240b932ec6543fc3e958bd0d370b2c5
H. Sabah <sabahhasbi00@gmail.com>
2023-07-15 10:29:50 -0700

    task 20

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 Module for the function Base
 """
 import json
+import csv
 
 
 class Base:
@@ -93,6 +94,50 @@ class Base:
                 inst = []
                 for item in lst:
                     inst.append(cls.create(**item))
+                return inst
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """
+        Writes a list of objects to a CSV file.
+
+        Args:
+            cls (class): The class itself.
+            list_objs (list): A list of objects to be written to the CSV file.
+        """
+        with open(cls.__name__ + ".csv", "w") as f:
+            wr = csv.writer(f)
+            for obj in list_objs:
+                if cls.__name__ == "Rectangle":
+                    wr.writerow([obj.id, obj.width, obj.height, obj.x, obj.y])
+                else:
+                    wr.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """
+        Loads objects from a CSV file and returns a list of instances.
+
+        Args:
+            cls (class): The class itself.
+        """
+        try:
+            with open(cls.__name__ + ".csv", "r") as f:
+                rd = csv.reader(f)
+                inst = []
+                r_keys = ["id", "width", "height", "x", "y"]
+                s_keys = ["id", "size", "x", "y"]
+                data = {}
+                for row in rd:
+                    if cls.__name__ == "Rectangle":
+                        for key, value in zip(r_keys, row):
+                            data[key] = int(value)
+                    else:
+                        for key, value in zip(s_keys, row):
+                            data[key] = int(value)
+                    inst.append(cls.create(**data))
                 return inst
         except FileNotFoundError:
             return []
