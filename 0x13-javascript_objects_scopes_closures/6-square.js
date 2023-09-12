9ebbeb72a814647cf5330f65e3a7712541ac5e4a
H. Sabah <sabahhasbi00@gmail.com>
2023-09-12 09:12:01 +0000

    Code fix: change inheritance from 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const SquareP = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends SquareP {
   constructor (size) {
     super(size, size);
   }
--- 0x13-javascript_objects_scopes_closures/main.js
+++ 0x13-javascript_objects_scopes_closures/main.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-const converter = require('./10-converter').converter;
-let myConverter = converter(10);
-console.log(myConverter(2));
-console.log(myConverter(12));
-console.log(myConverter(89));
-myConverter = converter(16);
-console.log(myConverter(2));
-console.log(myConverter(12));
-console.log(myConverter(89));
+const Square = require('./6-square');
+
+const s1 = new Square(4);
+s1.charPrint();
+
+s1.charPrint('C');
