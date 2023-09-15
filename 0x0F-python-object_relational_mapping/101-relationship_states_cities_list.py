994cd97108ecb8bf42e6c91e655741a82ef2d078
H. Sabah <sabahhasbi00@gmail.com>
2023-09-15 08:40:24 +0000

    Code fix: fixed pycodestyle

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -16,8 +16,8 @@ localhost:3306/{argv[3]}")
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(State).outerjoin(City).order_by(State.id, \
-City.id).all()
+    query = session.query(State).outerjoin(City)\
+        .order_by(State.id, City.id).all()
     for states in query:
         print(f"{states.id}: {states.name}")
         for city in states.cities:
