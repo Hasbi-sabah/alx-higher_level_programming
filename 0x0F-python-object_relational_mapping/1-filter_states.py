4ddc2c71e387aa495694dc803f3a1b7c5664ebe9
H. Sabah <sabahhasbi00@gmail.com>
2023-09-15 07:41:35 +0000

    Code fix: using LIKE BINARY instead of LIKE alone

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,12 +7,16 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = argv[3]
     db = MySQLdb.connect(
-        host="localhost", user=argv[1], passwd=argv[2], db=db, port=3306
+        host="localhost",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY \
+'N%' ORDER BY id")
     res = cursor.fetchall()
     for row in res:
         print(row)
