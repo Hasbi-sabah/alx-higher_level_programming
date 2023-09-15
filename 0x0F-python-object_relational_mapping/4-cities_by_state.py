2bd00fa42ec03fb79446bc3ead1be95096f252c2
H. Sabah <sabahhasbi00@gmail.com>
2023-09-15 07:45:57 +0000

    Code fix: replaced custom ip address with specified localhost

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,7 +5,7 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        host="172.17.0.2",
+        host="localhost",
         user=argv[1],
         passwd=argv[2],
         db=argv[3],
