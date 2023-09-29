a0a2c7da82952b28aa1c85c5a8f329e93ede3ede
H. Sabah <sabahhasbi00@gmail.com>
2023-09-29 16:00:53 +0000

    Code fix: pycodestyle

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,7 +6,8 @@ import sys
 if __name__ == "__main__":
     repo = sys.argv[1]
     user = sys.argv[2]
-    commits = requests.get(f"https://api.github.com/repos/{user}/{repo}/commits")
+    url = f"https://api.github.com/repos/{user}/{repo}/commits"
+    commits = requests.get(url)
     for ele in commits.json()[:10]:
         print(
             f"{ele.get('sha')}: \
