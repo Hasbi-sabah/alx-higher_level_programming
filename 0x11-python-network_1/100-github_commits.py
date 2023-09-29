07551e836e708ed9ccb1ce02cd98947988ac897e
H. Sabah <sabahhasbi00@gmail.com>
2023-09-29 15:57:49 +0000

    Files added: task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Python script that takes your GitHub credentials and displays your id """
+import requests
+import sys
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    user = sys.argv[2]
+    commits = requests.get(f"https://api.github.com/repos/{user}/{repo}/commits")
+    for ele in commits.json()[:10]:
+        print(
+            f"{ele.get('sha')}: \
+{ele.get('commit').get('author').get('name')}"
+        )
