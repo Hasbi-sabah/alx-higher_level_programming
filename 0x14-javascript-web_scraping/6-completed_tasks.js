b623bb7f63205900eac00591f46a38d9c9b763a6
H. Sabah <sabahhasbi00@gmail.com>
2023-10-24 13:16:44 +0000

    Code fix: even less hardcoding

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,7 +6,7 @@ const request = require('request');
 request(args[2], (err, code, body) => {
   if (err) { console.error(err); } else {
     const users = {};
-    for (let i = 1; i <= JSON.parse(body)[JSON.parse(body).length - 1].userId; i++) {
+    for (let i = JSON.parse(body)[0].userId; i <= JSON.parse(body)[JSON.parse(body).length - 1].userId; i++) {
       users[i] = JSON.parse(body).filter(task => task.completed && task.userId === i).length;
     }
     console.log(users);
