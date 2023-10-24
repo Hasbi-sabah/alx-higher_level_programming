febdc9a8362b93cafe38ddf698c2448106c8d8e4
H. Sabah <sabahhasbi00@gmail.com>
2023-10-24 12:07:07 +0000

    Code fix: the right way

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,11 +3,16 @@
 const args = process.argv;
 const request = require('request');
 
-const url = args[2].replace('films', 'people') + '/18';
-request(url, (err, code, body) => {
+request(args[2], (err, code, body) => {
   if (err) {
     console.error(err);
   } else {
-    console.log(JSON.parse(body).films.length);
+    let n = 0;
+    for (const film in JSON.parse(body).results) {
+      for (const character in JSON.parse(body).results[film].characters) {
+        if (JSON.parse(body).results[film].characters[character].includes('18')) { n += 1; }
+      }
+    }
+    console.log(n);
   }
 });
